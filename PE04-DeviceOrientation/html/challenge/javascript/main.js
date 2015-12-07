f9488c5e54ecb6619e8355d818511f2b728eddf9
leonardomra <leonardomra@yahoo.com.br>
2015-12-07 10:06:12 +0100

    Update main.js

--- PE04-DeviceOrientation/html/challenge/javascript/main.js
+++ PE04-DeviceOrientation/html/challenge/javascript/main.js
@@ -22,6 +22,14 @@
 /* HOMEWORK - implement collision detection by pasting the 4 chunks */
 /* of code in the right place (model.js, view.js, controller.js)    */
 /*------------------------------------------------------------------*/
+/*------------------------------------------------------------------*/
+/* IMPORTANT: Write 4 sentences next to the chunks you have pasted  */
+/* explaining the reasons of your devision.                         */
+/* Example: "I placed the following code inside the Model of the    */
+/* Pacman, because the model is responsible for processing data,    */
+/* and this function is related to..."                              */
+/*------------------------------------------------------------------*/
+/*------------------------------------------------------------------*/
 /* CHUNK A:                                                         */
 	/*
 	toBeEaten: function() {
