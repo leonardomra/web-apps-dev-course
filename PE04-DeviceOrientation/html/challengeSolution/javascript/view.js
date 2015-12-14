7e426af495cbf786fb3e3fdd955f4e03034a942e
leonardomra <leonardomra@yahoo.com.br>
2015-12-14 12:21:23 +0100

    Update view.js

--- PE04-DeviceOrientation/html/challengeSolution/javascript/view.js
+++ PE04-DeviceOrientation/html/challengeSolution/javascript/view.js
@@ -16,6 +16,11 @@ var View = {
 			this.element.style.top = elementYPos + 'px';
 			stage.appendChild(this.element);
 		},
+		// SOLUTION:
+		// As for the previous exercise, the balls are the ones that should disappear. However, now we don't achieve this by the user's click, but instead by collision detection performed by the model of the Pacman.
+		// As we want to keep this function flexible enough to still accept the user's click, we can make it flexible by accounting for different possibilities of the function's parameter. In this case, "e".
+		// We check if "e" is undefined, if so it means that there is no event being passed. 
+		// If there is no event, this function is not being used by an event listerner. So it is possible then to access the variable/key "element" with the path "this.element" in order to make it disappear.
 		disappear: function(e) {
 			var self = (e === undefined ? this.element : e.target);
 			self.style.display = 'none';
